7aeaaab0301e31e7d20b969775a21e0ceacbc406
sunyuhao <sunyuhao91482@gmail.com>
2017-06-30 09:42:53 +0200

    0630

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accueil.component.css']
 })
 export class AccueilComponent implements OnInit {
-
+title = 'test';
   constructor() { }
 
   ngOnInit() {
