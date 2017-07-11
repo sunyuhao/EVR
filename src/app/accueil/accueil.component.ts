7e050b75ae84bec31e8b5e85be0aa02b91ff75a9
Sun <yuhao.sun@yrbrands.com>
2017-07-11 15:34:19 +0200

    1107v2

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -47,33 +47,47 @@ checkLogin:CheckLogin;
   constructor( private router: Router,
     private accueilService: AccueilService) { }
 
+  ngOnInit(): void {
+      this.getLoginToken(this.login,this.pass);
+  }
+
+
   getLoginToken(login,pass):void{
-    this.accueilService.getLoginToken(login,pass).then(response => {this.token = response.token; this.getEvaluations(this.token,this.organizationId);this.getOrganisations(this.token);});
+    this.accueilService.getLoginToken(login,pass)
+    .then(response => {
+      this.token = response.token; 
+      this.getEvaluations(this.token,this.organizationId);
+      this.getOrganisations(this.token);
+    });
   }
 
   getEvaluations(token,organizationId): void {
-    this.accueilService.getEvaluations(token,organizationId).then(response => this.evaluations = response);
+    this.accueilService.getEvaluations(token,organizationId)
+    .then(response => this.evaluations = response);
   }
 
   getOneEvaluation(token,evaluationId): void {
-    this.accueilService.getOneEvaluation(token,evaluationId).then(response => this.oneEvaluation = response);
+    this.accueilService.getOneEvaluation(token,evaluationId)
+    .then(response => this.oneEvaluation = response);
   }
 
   getOrganisations(token): void {
-    this.accueilService.getOrganisations(token).then(response => this.organisations = response);
+    this.accueilService.getOrganisations(token)
+    .then(response => this.organisations = response);
   }
 
-  createEvaluation(token,evaluationName,mode="STD",evaluationId='458543'):void{
-    this.accueilService.createEvaluation(token,evaluationName,mode,evaluationId).then(response => this.oneEvaluation = response);
+  createEvaluation(evaluationName):void{
+    this.accueilService.createEvaluation(this.token,evaluationName,this.mode,this.organizationId)
+    .then(response => {
+      this.oneEvaluation = response;
+      this.router.navigate(['/metier']);
+    });
   }
 
 
   checkUser(token):void{
-     this.accueilService.checkUser(token).then(resp => this.checkLogin = resp);
-  }
-
-  add(evaluationName){
-    this.createEvaluation(this.token,evaluationName)
+     this.accueilService.checkUser(token)
+     .then(resp => this.checkLogin = resp);
   }
 
   getFileId(){
@@ -81,14 +95,5 @@ checkLogin:CheckLogin;
     console.log(this.fileId);
   }
 
-  ngOnInit(): void {
-  
-      this.getLoginToken(this.login,this.pass);
-     
-    //  this.getEvaluations(this.token);
-    // this.checkUser("");
-  }
-
-  get
 
 }
--- src/app/metier/metier.component.ts
+++ src/app/metier/metier.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router }            from '@angular/router';
 
+
+declare var jquery:any;
+declare var $ :any;
 @Component({
   selector: 'app-metier',
   templateUrl: './metier.component.html',
@@ -7,9 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MetierComponent implements OnInit {
   title="Duchuemin"
-  constructor() { }
+  constructor( private router: Router) { }
 
   ngOnInit() {
+
+    $('.working-situation-menu').mouseenter(function(){
+      $(this).children('.expand').addClass('turn');
+    });
+  
+    $('.working-situation-menu').mouseleave(function(){
+      if ($(this).hasClass('open')) {
+      } else {
+        $(this).children('.expand').removeClass('turn');
+      }
+    });
+    
+    $('.working-situation-menu').click(function () {
+      var $this = $(this);
+      if ($this.hasClass('open')) {
+        $('.working-situation-menu').removeClass('open');
+        $('.sub-menu').stop(true).slideUp("fast");
+        $('.expand').removeClass('turn');        
+        $this.removeClass('open');
+        $this.children('.expand').removeClass('turn');
+        $this.next().stop(true).slideUp("fast");
+      }    
+      else {
+        $('.working-situation-menu').removeClass('open');
+        $('.sub-menu').stop(true).slideUp("fast");
+        $('.expand').removeClass('turn');
+        
+        $this.addClass('open');
+        $this.children('.expand').addClass('turn');
+        $this.next().stop(true).slideDown("fast");
+        }
+    });
+
+    $(".checkAll").click(function(){
+    $('input:checkbox').not(this).prop('checked', this.checked);
+  });
+
+  }
+
+   gotoAccueil(): void {
+    this.router.navigate(['/accueil']);
   }
 
 }
